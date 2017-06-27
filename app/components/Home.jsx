75ab637f6bcb890a9d2f8951bc2784ca63c6a080
Abhinav Madahar <abhinavmadahar@gmail.com>
2017-06-27 19:35:59 +0000

    Add basic info

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 export default () => (
-  <div>
-    <header>
-      <h1>Abhinav Madahar</h1>
-    </header>
-    <img alt="" src="media/img/abhinav.jpg" />
+  <div className="container">
+    <div className="row">
+      <div className="eight columns">
+        <header>
+          <h1><u>Abhinav Madahar</u></h1>
+          <p>
+            <strong>Position:</strong> Rutgers University, undergraduate student<br />
+            <strong>Contact:</strong> <a href="mailto:abhinav.madahar@rutgers.edu">abhinav.madahar@rutgers.edu</a><br />
+            <strong>Updates:</strong> <a href="https://twitter.com/abhinavmadahar">my Twitter</a>
+          </p>
+        </header>
+      </div>
+      <div className="four columns">
+        <img alt="" src="media/img/abhinav.jpg" />
+      </div>
+    </div>
   </div>
 );
