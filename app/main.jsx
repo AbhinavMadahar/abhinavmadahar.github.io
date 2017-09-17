9bcdee145833524266f08cffc020bc1fbf5b06d0
Abhinav Madahar <abhinav.madahar@rutgers.edu>
2017-09-17 18:02:35 +0000

    Remove random 's'

--- app/main.jsx
+++ app/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import firebase from 'firebase';
 
-s
-
 import App from './components/App.jsx';
 import Home from './components/Home.jsx';
 
